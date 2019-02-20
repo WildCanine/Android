7e0ed0ead2718eeee10b133dca479564beb57f4a
WildCanine <matthew.woodall9090@gmail.com>
2019-02-20 12:00:49 -0700

    First Full Version

--- MWFirstAppAddition/app/src/main/java/com/example/mwfirstappaddition/MainActivity.java
+++ MWFirstAppAddition/app/src/main/java/com/example/mwfirstappaddition/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mwfirstappaddition;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addBtn=(Button) findViewById(R.id.addBtn);
+        Button subBtn=(Button) findViewById(R.id.subBtn);
+        Button mulBtn=(Button) findViewById(R.id.mulBtn);
+        Button divBtn=(Button) findViewById(R.id.divBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText=(EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText=(EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView=(TextView) findViewById(R.id.resultTextView);
+                int num1=Integer.parseInt(firstNumEditText.getText().toString());
+                int num2=Integer.parseInt(secondNumEditText.getText().toString());
+                int result=num1+num2;
+                resultTextView.setText(result+"");
+            }
+        });
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText=(EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText=(EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView=(TextView) findViewById(R.id.resultTextView);
+                int num1=Integer.parseInt(firstNumEditText.getText().toString());
+                int num2=Integer.parseInt(secondNumEditText.getText().toString());
+                int result=num1-num2;
+                resultTextView.setText(result+"");
+            }
+        });
+        mulBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText=(EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText=(EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView=(TextView) findViewById(R.id.resultTextView);
+                int num1=Integer.parseInt(firstNumEditText.getText().toString());
+                int num2=Integer.parseInt(secondNumEditText.getText().toString());
+                int result=num1*num2;
+                resultTextView.setText(result+"");
+            }
+        });
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText=(EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText=(EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView=(TextView) findViewById(R.id.resultTextView);
+                int num1=Integer.parseInt(firstNumEditText.getText().toString());
+                int num2=Integer.parseInt(secondNumEditText.getText().toString());
+                int result=num1/num2;
+                resultTextView.setText(result+"");
+            }
+        });
     }
 }
